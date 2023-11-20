831505b989b5725831e822b8bd6e90ff7cf72b7f
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-20 11:51:48 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SwiftUICoinExchange 
+## 스유 코인거래소 모의 프로젝트
---
+++ SwiftUICoinExchange/SwiftUICoinExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoinExchangeApp.swift
+//  SwiftUICoinExchange
+//
+//  Created by Taekwon Lee on 2023/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoinExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
