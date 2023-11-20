8f8647e1fba797dcf68ffc9612cff2062cec0ae0
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-20 12:04:33 +0900

    add: 거래소 화면 UI 초기 설정 - HStack, VStack

--- SwiftUICoinExchange/View/Lectures/231120/ContentView.swift
+++ SwiftUICoinExchange/View/Lectures/231120/ContentView.swift
@@ -9,11 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                bannerView()
+                LazyVStack {
+                    ForEach(1..<50) { data in
+                        listView(data: data)
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+    
+    
+    func bannerView() -> some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.gray)
+            .padding()
+            .frame(maxWidth: .infinity)
+            .frame(height: 200)
+    }
+    
+    func listView(data: Int) -> some View {
+        HStack {
+            VStack {
+                Text("코인 이름 \(data)")
+                    .bold()
+                Text("COIN NAME \(data)")
+                    .fontWeight(.light)
+            }
+            Spacer()
+            Text("KRW-BTC")
         }
         .padding()
     }
