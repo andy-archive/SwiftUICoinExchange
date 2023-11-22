f72b68975a721ab01d1570480a6d685e28d43630
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-22 09:45:45 +0900

    feat: struct immutable x capture list x escaping closures 실습

---
+++ SwiftUICoinExchange/View/Lectures/231121/RaceCondition/RaceCondition.swift
@@ -0,0 +1,19 @@
+//
+//  RaceCondition.swift
+//  SwiftUICoinExchange
+//
+//  Created by Taekwon Lee on 2023/11/21.
+//
+
+import Foundation
+
+//struct MyStruct {
+//    
+//    var myProperty: [Market]
+//    
+//    func mutateFunction() {
+//        UpbitAPI.fetchAllMarket { [self] market in // @escaping closure x capture list
+//            self.myProperty = market // 📌 Cannot assign to property: 'self' is an immutable capture
+//        }
+//    }
+//}
