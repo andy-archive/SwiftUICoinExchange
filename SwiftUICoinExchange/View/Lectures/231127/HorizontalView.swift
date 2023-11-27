21b11f38d6ee619ee75db16fffa159cfc7095e4e
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-27 11:54:48 +0900

    feat: GeometryReader, proxy를 통한 화면 크기 값 가져오기 및 활용

--- SwiftUICoinExchange/View/Lectures/231127/HorizontalView.swift
+++ SwiftUICoinExchange/View/Lectures/231127/HorizontalView.swift
@@ -10,24 +10,32 @@ import SwiftUI
 struct HorizontalView: View {
     var body: some View {
         ScrollView {
-            ForEach(currencyDummy, id: \.id) { item in
-                HStack(alignment: .center, spacing: 20) {
-                    Text(item.name)
-                        .frame(width: 100)
-                    ZStack(alignment: .center) {
-                        RoundedRectangle(cornerRadius: 15)
-                            .foregroundStyle(Color.blue.opacity(0.4))
-                            .frame(width: CGFloat(item.priceInUSD) / 10)
-                        Text(item.priceInUSD.formatted())
+            GeometryReader { proxy in
+                
+                let graphWidth = proxy.size
+                
+                VStack(spacing: 20) {
+                    ForEach(currencyDummy, id: \.id) { item in
+                        HStack(alignment: .center, spacing: 10) {
+                            Text(item.name)
+                                .frame(width: proxy.size.width * 0.2)
+                            ZStack(alignment: .center) {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .foregroundStyle(Color.blue.opacity(0.4))
+                                    .frame(width: CGFloat(item.priceInUSD) / 5)
+                                    .frame(maxWidth: proxy.size.width * 0.7)
+                                Text(item.priceInUSD.formatted())
+                            }
+                            .frame(maxWidth: .infinity)
+                            .background(Color.gray.opacity(0.6))
+                        }
                     }
-                    .frame(maxWidth: .infinity)
-                    .background(Color.gray.opacity(0.8))
                 }
-            }
-            .background(Color.yellow.opacity(0.2))
-            .onTapGesture {
-                print(currencyDummy)
-                print(showlargest())
+                .background(Color.yellow.opacity(0.2))
+                .onTapGesture {
+                    print(proxy)
+                    print(proxy.size)
+                }
             }
         }
     }
