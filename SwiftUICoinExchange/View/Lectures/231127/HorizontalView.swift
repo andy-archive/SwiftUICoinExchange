37b563e28b8ad7f5066164bd418e065c534220ca
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-28 10:24:44 +0900

    refactor: 어제 수업 복습 `@StateObject`, ObservableObject, `@Published`

--- SwiftUICoinExchange/View/Lectures/231127/HorizontalView.swift
+++ SwiftUICoinExchange/View/Lectures/231127/HorizontalView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HorizontalView: View {
     
+    /* View의 변화를 @StateObject를 통해 ViewModel에 전달 */
     @StateObject var viewModel = HorizontalViewModel()
     
     var body: some View {
@@ -21,23 +22,24 @@ struct HorizontalView: View {
             })
             GeometryReader { proxy in
                 
-                let graphWidth = proxy.size
+                let graphWidth = proxy.size.width
                 
                 VStack(spacing: 20) {
                     ForEach(currencyDummy, id: \.id) { item in
                         HStack(alignment: .center, spacing: 10) {
                             Text(item.name)
-                                .frame(width: proxy.size.width * 0.2)
-                            ZStack(alignment: .center) {
+                                .frame(width: graphWidth * 0.2)
+                            ZStack(alignment: .leading) {
                                 RoundedRectangle(cornerRadius: 15)
                                     .foregroundStyle(Color.blue.opacity(0.4))
                                     .frame(width: CGFloat(item.priceInUSD) / 5)
-                                    .frame(maxWidth: proxy.size.width * 0.7)
+                                    .frame(maxWidth: graphWidth * 0.7, alignment: .leading)
                                 Text(item.priceInUSD.formatted())
                             }
                             .frame(maxWidth: .infinity)
                             .background(Color.gray.opacity(0.6))
                         }
+                        .frame(height: 50)
                     }
                 }
                 .background(Color.yellow.opacity(0.2))
--- SwiftUICoinExchange/View/Lectures/231127/HorizontalViewModel.swift
+++ SwiftUICoinExchange/View/Lectures/231127/HorizontalViewModel.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
+/* ObservableObject 프로토콜을 이용하여 관찰자 시점 적용 */
 final class HorizontalViewModel: ObservableObject {
    
-    @Published var time = 0.0
+    @Published var time = 0.0 // View가 아니므로 @State가 아닌 @Published
     
     func timer() {
         Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { timer in
