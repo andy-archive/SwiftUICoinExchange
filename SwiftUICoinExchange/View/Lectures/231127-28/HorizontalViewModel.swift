4756bf6976c39f23acb2a71b8cd19e7c23d5a72b
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-28 12:17:54 +0900

    feat: VM에 네트워크 메서드 생성 성공 후 및 프로퍼티에 업데이트

--- SwiftUICoinExchange/View/Lectures/231127-28/HorizontalViewModel.swift
+++ SwiftUICoinExchange/View/Lectures/231127-28/HorizontalViewModel.swift
@@ -9,19 +9,20 @@ import Foundation
 
 /* ObservableObject 프로토콜을 이용하여 관찰자 시점 적용 */
 final class HorizontalViewModel: ObservableObject {
-   
+    
     @Published var time = 0.0 // View가 아니므로 @State가 아닌 @Published
     @Published var dummy: [Currency] = []
+    @Published var askOrderBook: [OrderBook] = []
+    @Published var bidOrderBook: [OrderBook] = []
     
     func timer() {
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { timer in
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { timer in
             self.time += 0.5
             self.fetchDummyData() // 0.5초마다 데이터 변화
         }
     }
     
     func fetchDummyData() {
-        
         dummy = [
             Currency(name: "ANDY"),
             Currency(name: "NOAH"),
@@ -37,9 +38,84 @@ final class HorizontalViewModel: ObservableObject {
         }
         return data[0].priceInUSD
     }
+    
+    func showlargestAskSize() -> Double {
+        let data = askOrderBook
+            .sorted {
+                $0.size > $1.size
+            }
+        return data.first?.size ?? 0
+    }
+    
+    func showlargestBidSize() -> Double {
+        let data = bidOrderBook
+            .sorted {
+                $0.size > $1.size
+            }
+        return data.first?.size ?? 0
+    }
+    
+    func fetchOrderbook() {
+        guard let url = URL(string: "https://api.upbit.com/v1/orderbook?markets=BTC-ETH") else { return }
         
+        print(url)
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            print(response)
+            
+            if let error {
+                
+                let decoder = JSONDecoder()
+                
+                guard let data = data else {
+                    print("\(error.localizedDescription)")
+                    return
+                }
+                
+                do {
+                    let decodedData = try decoder.decode([OrderBookResponseDTO].self, from: data)
+                    
+                    DispatchQueue.main.async {
+                        
+                        let result = decodedData.first?.orderbookUnits ??
+                        [OrderBookUnit(
+                            askPrice: 0,
+                            bidPrice: 0,
+                            askSize: 0.0,
+                            bidSize: 0.0
+                        )]
+                        
+                        let ask = result
+                            .map { unit in
+                                OrderBook(
+                                    price: unit.askPrice,
+                                    size: unit.askSize
+                                )
+                            }
+                            .sorted { $0.price > $1.price }
+                        
+                        let bid = result
+                            .map { unit in
+                                OrderBook(
+                                    price: unit.bidPrice,
+                                    size: unit.bidSize
+                                )
+                            }
+                            .sorted { $0.price > $1.price }
+                        
+                        self.askOrderBook = ask
+                        self.bidOrderBook = bid
+                    }
+                    
+                    print("COMPLETE ---------------------")
+                } catch {
+                    print("\(error.localizedDescription)")
+                }
             }
         }
+        
+        task.resume()
     }
 }
 
