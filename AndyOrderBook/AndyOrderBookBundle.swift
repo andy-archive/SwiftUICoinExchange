4be3b920fd06d81e94ca94f9462a5104ac5fff01
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-11-29 11:00:02 +0900

    chore: WidgetKit Extension 추가

---
+++ AndyOrderBook/AndyOrderBookBundle.swift
@@ -0,0 +1,16 @@
+//
+//  AndyOrderBookBundle.swift
+//  AndyOrderBook
+//
+//  Created by Taekwon Lee on 11/29/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct AndyOrderBookBundle: WidgetBundle {
+    var body: some Widget {
+        AndyOrderBook()
+    }
+}
